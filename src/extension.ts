cf5c63a8bfa551828dcf52736f271aca3a7e0bbd
Gerard Braad <me@gbraad.nl>
2025-05-17 15:30:39 +0800

    Fix small order issue in command name

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,13 @@ import { selectDevenv, runDevenvSystem, runDevenvNoinit, runDevenvStart, runDeve
 export function activate(context: vscode.ExtensionContext) {
     Log.info(`Activating dotfiles tools`);
 
+    // Commands related to: `devenv [prefix] [command]`
     context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.selectDevenv', selectDevenv));
     context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvStart', runDevenvStart));
     context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvSystem', runDevenvSystem));
     context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvNoinit', runDevenvNoinit));
-    context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvShellUser', runDevenvUserShell));
-    context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvShellRoot', runDevenvRootShell));
+    context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvUserShell', runDevenvUserShell));
+    context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvRootShell', runDevenvRootShell));
     context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvUserEnv', runDevenvUserEnv));
     context.subscriptions.push(vscode.commands.registerCommand('devenv-helper.runDevenvRootEnv', runDevenvRootEnv));
 }
